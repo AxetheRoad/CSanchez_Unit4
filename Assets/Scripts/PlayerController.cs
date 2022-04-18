f1b07a8eb170852ee0b72afdd9484a9ab6a1694e
AxetheRoad <AxetheRoad@hotmail.com>
2022-04-17 23:24:59 -0700

    Lesson B

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody rbPlayer;
     GameObject focalPoint;
     Renderer renderPlayer;
-    public float speed = 10f;
+    public float speed = 50f;
    
     // Start is called before the first frame update
     void Start()
@@ -22,12 +22,15 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         float magnitude = forwardInput * speed * Time.deltaTime;
-        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Impulse);
+        rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Force);
 
         if (forwardInput > 0)
         {
             renderPlayer.material.color = new Color(1.0f - forwardInput, 1.0f, 1.0f - forwardInput);
         }
-       
+       else
+        {
+            renderPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
+        }
     }
 }
